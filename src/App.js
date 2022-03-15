eeb3ec8007eb2bdd5dbbf901174d990c9f77afce
ShvachkinArtem <shwachkinartem@gmail.com>
2022-03-15 17:35:24 +0300

    Remaking front-end layout)

--- src/App.js
+++ src/App.js
@@ -1,157 +1,232 @@
-
-import ToDoList from './tasks';
-import { useEffect, useState } from "react"
-
+import ToDoList from "./tasks";
+import { useEffect, useState } from "react";
+import { Button, Col, Row, Typography, Input, Space , Pagination } from "antd";
+import "antd/dist/antd.css";
+import "./index.css";
 const App = () => {
-  const [todos, setTodos] = useState([])
-  const [count, setCount] = useState([])
-  const [filterSort, setFilter] = useState([])
-  const [newTitle, setTitle] = useState("")
-  const [newPage, setPage] = useState(0)
-  const [filter, setnewFilter] = useState('ALL');
+  const [todos, setTodos] = useState([]);
+  const [count, setCount] = useState([]);
+  const [filterSort, setFilter] = useState([]);
+  const [newTitle, setTitle] = useState("");
+  const [newPage, setPage] = useState(0);
+  const [filter, setnewFilter] = useState("ALL");
 
   const addTodo = () => {
-    setTodos(prev => [...prev, { id: newId, status: false, title: newTitle, createdAt: new Date().getTime() }])
-  }
+    setTodos((prev) => [
+      ...prev,
+      {
+        id: newId,
+        status: false,
+        title: newTitle,
+        createdAt: new Date().getTime(),
+      },
+    ]);
+  };
 
   useEffect(() => {
     setCount(todos.length);
 
-    const sliceTodos = ([...todos].slice(5 * newPage, 5 * newPage + 5))
+    const sliceTodos = [...todos].slice(5 * newPage, 5 * newPage + 5);
 
-    if(newPage>0 && sliceTodos.length === 0){
-      setPage(prev => prev-1)
-      return
+    if (newPage > 0 && sliceTodos.length === 0) {
+      setPage((prev) => prev - 1);
+      return;
     }
 
-    if (filter === 'ALL') {
+    if (filter === "ALL") {
       setFilter([...sliceTodos]);
       return;
     }
 
-    if (filter === 'DONE') {
-      setFilter([...sliceTodos].filter(todos => todos.status === true))
+    if (filter === "DONE") {
+      setFilter([...sliceTodos].filter((todos) => todos.status === true));
       return;
     }
 
-
-    if (filter === 'UNDONE') {
-      setFilter([...sliceTodos].filter(todos => todos.status === false))
+    if (filter === "UNDONE") {
+      setFilter([...sliceTodos].filter((todos) => todos.status === false));
       return;
     }
+  }, [filter, todos, newPage]);
 
-  }, [filter, todos, newPage])
-
-  const [newId, setId] = useState(0)
+  const [newId, setId] = useState(0);
   const toggleTodo = (id, newStatus) => {
-    setTodos(prev => prev.map(el => {
-      if (el.id === id) {
-        return {
-          ...el,
-          status: newStatus
+    setTodos((prev) =>
+      prev.map((el) => {
+        if (el.id === id) {
+          return {
+            ...el,
+            status: newStatus,
+          };
         }
-      }
-      return el
-    }))
-  }
+        return el;
+      })
+    );
+  };
   const handleChange = ({ target }) => {
-    setTitle(target.value)
-  }
+    setTitle(target.value);
+  };
   const handleButton = ({ key }) => {
     if (key === "Enter") {
-      if (newTitle.trim().length === 0) { return };
+      if (newTitle.trim().length === 0) {
+        return;
+      }
       addTodo();
       setTitle("");
-      setId(newId + 1); console.log(newId)
+      setId(newId + 1);
     }
-
-  }
+  };
   const statusCheck = (status) => {
-    setFilter([...todos].filter(todos => todos.status === status))
-  }
+    setFilter([...todos].filter((todos) => todos.status === status));
+  };
   const changeTitle = (id, nextTitle) => {
-    const index = todos.findIndex(todo => todo.id === id);
+    const index = todos.findIndex((todo) => todo.id === id);
     const newTodos = [...todos];
     newTodos[index].title = nextTitle;
-    setTodos(newTodos)
-  }
+    setTodos(newTodos);
+  };
   const deleteTask = (id) => {
-    setTodos(prev => prev.filter(todos => todos.id !== id))
-  }
+    setTodos((prev) => prev.filter((todos) => todos.id !== id));
+  };
   const sortbydate = (up) => {
     if (up) {
-      setTodos([...todos].sort(function (a, b) {
-        return a.createdAt - b.createdAt;
-      }))
-      return
+      setTodos(
+        [...todos].sort(function (a, b) {
+          return a.createdAt - b.createdAt;
+        })
+      );
+      return;
     }
-    setTodos([...todos].sort(function (a, b) {
-      return b.createdAt - a.createdAt;
-    }))
-  }
-
-  const countPages = Math.ceil(count/5);
-
-  const paginationButtons = [] 
-  for(let i = 0 ;i<countPages;i++) {
-    paginationButtons.push(<button onClick={() => setPage(i)} className="pagination-button">{i+1}</button>)
-  }
-
-  const pagination = count > 5 ? (<div className="pagination-wrapper">
-    <button className="pagination-button" onClick={() => setPage(0)}> {"<<"}</button>
-    {paginationButtons}
-    <button className="pagination-button" onClick={() => setPage(countPages - 1)}>{">>"}</button>
-  </div>) : <></>
+    setTodos(
+      [...todos].sort(function (a, b) {
+        return b.createdAt - a.createdAt;
+      })
+    );
+  };
 
   return (
     <div>
-      <h1 className="header">
-        ToDo
-      </h1>
+      <Row justify="center">
+        <Col span={2}>
+          {" "}
+          <Typography.Title
+            level={1}
+            style={{ fontSize: "60px", fontWeight: 600, margin: 0 }}
+          >
+            ToDo
+          </Typography.Title>
+        </Col>
+      </Row>
+
       <div className="dowhat">
-        <input value={newTitle} onChange={handleChange} onKeyDown={handleButton} className="wanttodo" type="text" placeholder="I want to do..." />
+        <Row justify="center">
+          <Col span={8}>
+            <Input
+              style={{ fontSize: "25px", fontWeight: 800, margin: 0 }}
+              size="large"
+              placeholder="I want to do..."
+              value={newTitle}
+              onChange={handleChange}
+              onKeyDown={handleButton}
+              className="wanttodo"
+              type="text"
+            />
+          </Col>
+        </Row>
       </div>
       <div className="options">
-        <div className="buttons">
-          <button onClick={() => setnewFilter('ALL')} className="filter-button" >All</button>
-          <button onClick={() => setnewFilter('DONE')} className="filter-button">Done  </button>
-          <button onClick={() => setnewFilter('UNDONE')} className="filter-button">Undone  </button>
-        </div>
-        <div className="sortbydate">
-          <p style={{ marginRight: "5px" }}>Sort by date</p>
-          <div className="arrows">
-            <button >
-              <div onClick={() => sortbydate(true)} className="arrowup">
-                <svg width="20px" height="20px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M8.2 751.4c0 8.6 3.4 17.401 10 24.001 13.2 13.2 34.8 13.2 48 0l451.8-451.8 445.2 445.2c13.2 13.2 34.8 13.2 48 0s13.2-34.8 0-48L542 251.401c-13.2-13.2-34.8-13.2-48 0l-475.8 475.8c-6.8 6.8-10 15.4-10 24.2z" />
-                </svg>
-
-              </div>
-            </button>
-            <button>
-              <div onClick={() => sortbydate(false)} className="arrowdown">
-                <svg width="20px" height="20px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M8.2 751.4c0 8.6 3.4 17.401 10 24.001 13.2 13.2 34.8 13.2 48 0l451.8-451.8 445.2 445.2c13.2 13.2 34.8 13.2 48 0s13.2-34.8 0-48L542 251.401c-13.2-13.2-34.8-13.2-48 0l-475.8 475.8c-6.8 6.8-10 15.4-10 24.2z" />
-                </svg>
-
-              </div>
-            </button>
+        
+        <Row justify="space-around" style={{marginTop:'50px'}}>
+          <div className="buttons">
+            <Row>
+              <Col>
+            <Button size="large" style={{ fontSize: "20px", fontWeight: 800 }}
+              type="default"
+              onClick={() => setnewFilter("ALL")}
+              className="filter-button"
+            >
+              All
+            </Button>
+            </Col>
+            <Col>
+            <Button size="large" style={{ fontSize: "20px", fontWeight: 800}}
+              type="default"
+              onClick={() => setnewFilter("DONE")}
+              className="filter-button"
+            >
+              Done{" "}
+            </Button>
+            </Col>
+            <Col>
+            <Button size="large" style={{  fontSize: "20px", fontWeight: 800 }}
+              type="default"
+              onClick={() => setnewFilter("UNDONE")}
+              className="filter-button"
+            >
+              Undone{" "}
+            </Button>
+            </Col>
+            </Row>
           </div>
-        </div>
+          <div className="sortbydate">
+          
+            <Row>
+            <Space>
+            <Typography.Title level={1} style={{ margin: 0 }}>
+              Sort by date
+            </Typography.Title>
+          
 
+            <div className="arrows">
+              <Button type="default">
+                <div onClick={() => sortbydate(true)} className="arrowup">
+                  <svg
+                    width="20px"
+                    height="20px"
+                    viewBox="0 0 1024 1024"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M8.2 751.4c0 8.6 3.4 17.401 10 24.001 13.2 13.2 34.8 13.2 48 0l451.8-451.8 445.2 445.2c13.2 13.2 34.8 13.2 48 0s13.2-34.8 0-48L542 251.401c-13.2-13.2-34.8-13.2-48 0l-475.8 475.8c-6.8 6.8-10 15.4-10 24.2z" />
+                  </svg>
+                </div>
+              </Button>
+              <Button type="default">
+                <div onClick={() => sortbydate(false)} className="arrowdown">
+                  <svg
+                    width="20px"
+                    height="20px"
+                    viewBox="0 0 1024 1024"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M8.2 751.4c0 8.6 3.4 17.401 10 24.001 13.2 13.2 34.8 13.2 48 0l451.8-451.8 445.2 445.2c13.2 13.2 34.8 13.2 48 0s13.2-34.8 0-48L542 251.401c-13.2-13.2-34.8-13.2-48 0l-475.8 475.8c-6.8 6.8-10 15.4-10 24.2z" />
+                  </svg>
+                </div>
+              </Button>
+            </div>
+            </Space>
+
+            </Row>
+          </div>
+          
+        </Row>
+        
       </div>
 
+      <ToDoList
+        todos={filterSort}
+        onToggle={toggleTodo}
+        deleteTask={deleteTask}
+        changeTitle={changeTitle}
+        setFilter={setnewFilter}
+      ></ToDoList>
 
-      <ToDoList todos={filterSort} onToggle={toggleTodo} deleteTask={deleteTask} changeTitle={changeTitle} setFilter={setnewFilter}></ToDoList>
-
-
-
-{pagination}
-
+<Pagination>
+        total={count}
+    defaultCurrent={1}
+    onChange={(page)=>setPage(page)}
+    </Pagination>
     </div>
   );
-}
+};
 
 export default App;
--- src/todoItem.js
+++ src/todoItem.js
@@ -1,42 +1,77 @@
-import { useState } from "react"
+import { useState } from "react";
+import { Row, Col , Card } from "antd";
+import Checkbox from "antd/lib/checkbox/Checkbox";
 
-
- const TodoItem = ({ onChange, todos, deleteTask, setTodos }) => {
-
-    const [editTask, setTask] = useState(todos.title);
-    const [editStatus, setStatus] = useState(false);
-    const handleTask = ({ key }) => {
-        if (key === "Enter") {
-            if (editTask.trim().length === 0) { return };
-            setTodos(todos.id, editTask);
-            setStatus(false)
-        }
+const TodoItem = ({ onChange, todo, deleteTask, setTodos }) => {
+  const [editTask, setTask] = useState(todo.title);
+  const [editStatus, setStatus] = useState(false);
+  const handleTask = ({ key }) => {
+    if (key === "Enter") {
+      if (editTask.trim().length === 0) {
+        return;
+      }
+      setTodos(todo.id, editTask);
+      setStatus(false);
     }
-    const taskelement = editStatus ? <input maxLength={33} className={editTask.length === 0 ? "trumbleA" : ""} value={editTask} onChange={e => setTask(e.target.value)} onKeyDown={handleTask}></input> : <p onClick={() => setStatus(true)}>{todos.title} </p>
+  };
+  const taskelement = editStatus ? (
+    <input
+      maxLength={33}
+      className={editTask.length === 0 ? "trumbleA" : ""}
+      value={editTask}
+      onChange={(e) => setTask(e.target.value)}
+      onKeyDown={handleTask}
+    ></input>
+  ) : (
+    <p onClick={() => setStatus(true)}>{todo.title} </p>
+  );
 
-    return (
-        <div className="todoTasks">
-            <div className="check">
-                <input type="checkbox" value={todos.status} onChange={({ target }) => onChange(todos.id, target.checked)} checked={todos.status} />
-                {taskelement}
-            </div>
-            <div className="delete">
-                <p>{new Date(todos.createdAt).toLocaleString()}</p>
-                <button className="delete-todo" onClick={() => deleteTask(todos.id)}>
-                    <svg className="trashcan" width="32px" height="32px" viewBox="0 0 32 32" id="icon" xmlns="http://www.w3.org/2000/svg" >
-                        <defs>
-                        </defs>
-                        <title>trash-can</title>
-                        <rect x="12" y="12" width="2" height="12" />
-                        <rect x="18" y="12" width="2" height="12" />
-                        <path d="M4,6V8H6V28a2,2,0,0,0,2,2H24a2,2,0,0,0,2-2V8h2V6ZM8,28V8H24V28Z" />
-                        <rect x="12" y="2" width="8" height="2" />
-                        <rect style={{ fill: 'none' }} id="_Transparent_Rectangle_" data-name="&lt;Transparent Rectangle&gt;" className="cls-1"
-                            width="32" height="32" />
-                    </svg>
-                </button>
-            </div>
-        </div>
-    )
-}
-export default TodoItem
+  return (
+    <div className="todoTasks">
+        
+        <Card style={{}}>
+      <div className="check"></div>
+      <Row justify="space-around">
+        <Col span={8}>
+          {" "}
+          <Checkbox
+            value={todo.status}
+            onChange={({ target }) => onChange(todo.id, target.checked)}
+            checked={todo.status}
+          />
+          {taskelement}
+        </Col>
+        <Col span={8}>
+          <p>{new Date(todo.createdAt).toLocaleString()}</p>
+          <button className="delete-todo" onClick={() => deleteTask(todo.id)}>
+            <svg
+              className="trashcan"
+              width="32px"
+              height="32px"
+              viewBox="0 0 32 32"
+              id="icon"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <defs></defs>
+              <title>trash-can</title>
+              <rect x="12" y="12" width="2" height="12" />
+              <rect x="18" y="12" width="2" height="12" />
+              <path d="M4,6V8H6V28a2,2,0,0,0,2,2H24a2,2,0,0,0,2-2V8h2V6ZM8,28V8H24V28Z" />
+              <rect x="12" y="2" width="8" height="2" />
+              <rect
+                style={{ fill: "none" }}
+                id="_Transparent_Rectangle_"
+                data-name="&lt;Transparent Rectangle&gt;"
+                className="cls-1"
+                width="32"
+                height="32"
+              />
+            </svg>
+          </button>
+        </Col>
+      </Row>
+      </Card>
+    </div>
+  );
+};
+export default TodoItem;
